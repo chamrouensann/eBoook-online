60091c39d76d715fc73ce1673ba80f39be1fba9c
chamrouensann <chamrouensann.geek@gmail.com>
2020-08-21 00:02:50 +0700

    Finish the landing page

--- ebook_online_source_code/lib/main.dart
+++ ebook_online_source_code/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ebookonlinesourcecode/constants.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,10 +8,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Book Application',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: Theme.of(context).textTheme.apply(
+              displayColor: kBlackColor,
+            ),
       ),
       home: WelcomeScreen(),
     );
@@ -20,6 +24,82 @@ class MyApp extends StatelessWidget {
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/Bitmap.png"),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RichText(
+              text: TextSpan(
+                  style: Theme.of(context).textTheme.display3,
+                  children: [
+                    TextSpan(text: "flamin"),
+                    TextSpan(
+                        text: "go.",
+                        style: TextStyle(fontWeight: FontWeight.bold))
+                  ]),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * .6,
+              child: RoundedButton(
+                text: "Start Reading",
+                fontSize: 20,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RoundedButton extends StatelessWidget {
+  final String text;
+  final Function press;
+  final double verticalPadding;
+  final double fontSize;
+  const RoundedButton({
+    Key key,
+    this.text,
+    this.press,
+    this.verticalPadding = 16,
+    this.fontSize = 16,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.symmetric(vertical: 16),
+        padding:
+            EdgeInsets.symmetric(vertical: verticalPadding, horizontal: 30),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(30),
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 15),
+                blurRadius: 30,
+                color: Color(0xFF666666).withOpacity(.11),
+              ),
+            ]),
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: fontSize,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
