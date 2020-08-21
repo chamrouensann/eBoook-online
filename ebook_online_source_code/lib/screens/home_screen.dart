9205a28dd3c853ce207be9567655088333b678c0
chamrouensann <chamrouensann.geek@gmail.com>
2020-08-22 00:02:19 +0700

    Adding what are you reading check box

--- ebook_online_source_code/lib/screens/home_screen.dart
+++ ebook_online_source_code/lib/screens/home_screen.dart
@@ -1,10 +1,72 @@
+import 'package:ebookonlinesourcecode/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(),
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/main_page_bg.png"),
+            alignment: Alignment.topCenter,
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: size.height * .1),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: RichText(
+                text: TextSpan(
+                  style: Theme.of(context).textTheme.display1,
+                  children: [
+                    TextSpan(text: "What are you \nreading "),
+                    TextSpan(
+                        text: "today?",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ))
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Container(
+              height: 245,
+              width: 202,
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      height: 221,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(29),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 10),
+                            blurRadius: 33,
+                            color: kShadowColor,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Image.asset("assets/images/book-1.png"),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
