7004f6684eb1a38bd9b75aa18b75c88cd99b5fac
chamrouensann <chamrouensann.geek@gmail.com>
2020-08-25 16:04:06 +0700

    Adding reading cart list widgets

--- ebook_online_source_code/lib/screens/home_screen.dart
+++ ebook_online_source_code/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:ebookonlinesourcecode/constants.dart';
+import 'package:ebookonlinesourcecode/widgets/reading_cart_list.dart';
 import 'package:ebookonlinesourcecode/widgets/two_side_rounded_button.dart';
 import 'package:flutter/material.dart';
 import 'package:ebookonlinesourcecode/widgets/book_rating.dart';
@@ -38,132 +39,45 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             SizedBox(height: 10),
-            ReadingListCard(
-              image: "assets/images/book-1.png",
-              title: "Crushing & Influence",
-              auth: "Rouen Pro",
-              rating: 4.7,
-            )
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: <Widget>[
+                  ReadingListCard(
+                    image: "assets/images/book-1.png",
+                    title: "Crushing & Influence",
+                    auth: "Rouen Pro",
+                    rating: 4.7,
+                  ),
+                  ReadingListCard(
+                    image: "assets/images/book-2.png",
+                    title: "Top 10 Business hack",
+                    auth: "Dara Lay",
+                    rating: 4.7,
+                  ),
+                  ReadingListCard(
+                    image: "assets/images/book-3.png",
+                    title: "How to win friends",
+                    auth: "Hong Kimvai",
+                    rating: 4.7,
+                  ),
+                  ReadingListCard(
+                    image: "assets/images/book-1.png",
+                    title: "Crushing & Influence",
+                    auth: "Hong Kimvai",
+                    rating: 4.7,
+                  ),
+                  ReadingListCard(
+                    image: "assets/images/book-1.png",
+                    title: "Crushing & Influence",
+                    auth: "Rouen Pro",
+                    rating: 4.7,
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class ReadingListCard extends StatelessWidget {
-  final String image;
-  final String title;
-  final String auth;
-  final double rating;
-  final Function pressDetails;
-  final Function pressRead;
-
-  const ReadingListCard({
-    Key key,
-    this.image,
-    this.title,
-    this.auth,
-    this.rating,
-    this.pressDetails,
-    this.pressRead,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(left: 24, bottom: 40),
-      height: 300,
-      width: 210,
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-            bottom: 10,
-            left: 10,
-            right: 0,
-            child: Container(
-              height: 221,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(29),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 10),
-                    blurRadius: 33,
-                    color: kShadowColor,
-                  )
-                ],
-              ),
-            ),
-          ),
-          Image.asset(image),
-          Positioned(
-            top: 75,
-            right: 0,
-            child: Column(
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.favorite_border),
-                  onPressed: () {},
-                ),
-                BookRating(
-                  score: rating,
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            top: 200,
-            child: Container(
-              height: 85,
-              width: 202,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(left: 24),
-                    child: RichText(
-                      maxLines: 2,
-                      text: TextSpan(
-                        style: TextStyle(color: kBlackColor),
-                        children: [
-                          TextSpan(
-                            text: "$title\n",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          TextSpan(
-                            text: auth,
-                            style: TextStyle(color: kLightBlackColor),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  Spacer(),
-                  Row(
-                    children: <Widget>[
-                      GestureDetector(
-                        onTap: pressDetails,
-                        child: Container(
-                          width: 101,
-                          padding: EdgeInsets.symmetric(vertical: 10),
-                          alignment: Alignment.center,
-                          child: Text("Details"),
-                        ),
-                      ),
-                      Expanded(
-                        child: TwoSideRoundedButton(
-                          text: "Read",
-                          press: pressRead,
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
       ),
     );
   }
