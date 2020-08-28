b3709a583ddbec5538839349dcdf1e72984ea311
chamrouensann <chamrouensann.geek@gmail.com>
2020-08-28 18:31:45 +0700

    Completed the entire project

--- ebook_online_source_code/lib/screens/details_screen.dart
+++ ebook_online_source_code/lib/screens/details_screen.dart
@@ -10,6 +10,7 @@ class DetailsScreen extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Stack(
               alignment: Alignment.topCenter,
@@ -32,72 +33,277 @@ class DetailsScreen extends StatelessWidget {
                     child: Column(
                       children: <Widget>[
                         SizedBox(height: size.height * .1),
-                        Row(
-                          children: <Widget>[
-                            Expanded(
-                              child: Column(
-                                children: <Widget>[
-                                  Text(
-                                    "Crushing &",
-                                    style: Theme.of(context).textTheme.display1,
-                                  ),
-                                  Text(
-                                    "Influence",
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .display1
-                                        .copyWith(fontWeight: FontWeight.bold),
-                                  ),
-                                  Row(
-                                    children: <Widget>[
-                                      Expanded(
-                                        child: Column(
-                                          children: <Widget>[
-                                            Text(
-                                              "August has a special hold on those of us who are incurable romantics. Somewhere between daydreaming in the sun and gazing at the Perseid meteor shower at night, we chase the last glimpse of summer in swoon-worthy romance novels. August has a special hold on those of us who are incurable romantics. Somewhere between daydreaming in the sun and gazing at the Perseid meteor shower at night, we chase the last glimpse of summer in swoon-worthy romance novels.August has a special hold on those of us who are incurable romantics. Somewhere between daydreaming in the sun and gazing at the Perseid meteor shower at night, we chase the last glimpse of summer in swoon-worthy romance novels.",
-                                              maxLines: 6,
-                                              style: TextStyle(
-                                                fontSize: 14,
-                                                color: kLightBlackColor,
-                                              ),
-                                            ),
-                                            SizedBox(height: 5),
-                                            RoundedButton(
-                                              text: "Read More",
-                                              verticalPadding: 12,
-                                            )
-                                          ],
-                                        ),
-                                      ),
-                                      Column(
-                                        children: <Widget>[
-                                          IconButton(
-                                            icon: Icon(Icons.favorite_border),
-                                            onPressed: () {},
-                                          ),
-                                          BookRating(score: 5.0),
-                                        ],
-                                      )
-                                    ],
-                                  )
-                                ],
-                              ),
-                            ),
-                            Image.asset(
-                              "assets/images/book-1.png",
-                              height: 200,
-                            )
-                          ],
-                        )
+                        BookInfo()
                       ],
                     ),
                   ),
                 ),
+                Padding(
+                  padding: EdgeInsets.only(top: size.height * .4 - 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      ChapterCard(
+                        name: "Life",
+                        chapterNumber: 1,
+                        tag: "Love your life and changing",
+                        press: () {},
+                      ),
+                      ChapterCard(
+                        name: "Changing",
+                        chapterNumber: 2,
+                        tag: "Sometimes we good to change the habit",
+                        press: () {},
+                      ),
+                      ChapterCard(
+                        name: "My Parents",
+                        chapterNumber: 3,
+                        tag: "Take care your parents",
+                        press: () {},
+                      ),
+                      ChapterCard(
+                        name: "Planning",
+                        chapterNumber: 4,
+                        tag: "Make a plan and do it",
+                        press: () {},
+                      ),
+                      SizedBox(
+                        height: 10,
+                      )
+                    ],
+                  ),
+                )
               ],
             ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                children: <Widget>[
+                  RichText(
+                    text: TextSpan(
+                        style: Theme.of(context).textTheme.display1,
+                        children: [
+                          TextSpan(text: "You might also "),
+                          TextSpan(
+                            text: "like...",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ]),
+                  ),
+                  SizedBox(height: 20),
+                  Stack(
+                    children: <Widget>[
+                      Container(
+                        height: 180,
+                        width: double.infinity,
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          padding:
+                              EdgeInsets.only(left: 24, top: 24, right: 150),
+                          height: 168,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(29),
+                            color: Color(0xFFFFF8F9),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              RichText(
+                                text: TextSpan(
+                                  style: TextStyle(color: kBlackColor),
+                                  children: [
+                                    TextSpan(
+                                      text:
+                                          "How to win friends and Influence people \n",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: "Rouen Pro",
+                                      style: TextStyle(color: kLightBlackColor),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  BookRating(
+                                    score: 4.8,
+                                  ),
+                                  SizedBox(width: 20),
+                                  Expanded(
+                                    child: RoundedButton(
+                                      text: "Read",
+                                      verticalPadding: 14,
+                                    ),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: Image.asset(
+                          "assets/images/book-2.png",
+                          width: 150,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            )
           ],
         ),
       ),
+    );
+  }
+}
+
+class ChapterCard extends StatelessWidget {
+  final String name;
+  final String tag;
+  final int chapterNumber;
+  final Function press;
+  const ChapterCard({
+    Key key,
+    this.name,
+    this.tag,
+    this.chapterNumber,
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+      margin: EdgeInsets.only(bottom: 16),
+      width: size.width - 48,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(38.5),
+        boxShadow: [
+          BoxShadow(
+              offset: Offset(0, 10),
+              blurRadius: 33,
+              color: Color(0xFFD3D3D3).withOpacity(.84))
+        ],
+      ),
+      child: Row(
+        children: <Widget>[
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "Chapter $chapterNumber: $name\n",
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: kBlackColor,
+                      fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                    text: tag,
+                    style: TextStyle(
+                      color: kBlackColor,
+                    ))
+              ],
+            ),
+          ),
+          Spacer(),
+          IconButton(
+            icon: Icon(
+              Icons.arrow_forward_ios,
+              size: 18,
+            ),
+            onPressed: press,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BookInfo extends StatelessWidget {
+  const BookInfo({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            children: <Widget>[
+              Text(
+                "Crushing &",
+                style: Theme.of(context).textTheme.display1,
+              ),
+              Text(
+                "Influence",
+                style: Theme.of(context)
+                    .textTheme
+                    .display1
+                    .copyWith(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 5),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "August has a special hold on those of us who are incurable romantics. Somewhere between daydreaming in the sun and gazing at the Perseid meteor shower at night, we chase the last glimpse of summer in swoon-worthy romance novels. August has a special hold on those of us who are incurable romantics. Somewhere between daydreaming in the sun and gazing at the Perseid meteor shower at night, we chase the last glimpse of summer in swoon-worthy romance novels.August has a special hold on those of us who are incurable romantics. Somewhere between daydreaming in the sun and gazing at the Perseid meteor shower at night, we chase the last glimpse of summer in swoon-worthy romance novels.",
+                          maxLines: 6,
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: kLightBlackColor,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        RoundedButton(
+                          text: "Read More",
+                          verticalPadding: 12,
+                        )
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.favorite_border),
+                        onPressed: () {},
+                      ),
+                      BookRating(score: 5.0),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+        Image.asset(
+          "assets/images/book-1.png",
+          height: 200,
+        )
+      ],
     );
   }
 }
